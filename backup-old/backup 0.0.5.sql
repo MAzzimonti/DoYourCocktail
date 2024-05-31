5b991f3912963baaf59e0aa3c43d1893fa40898c
MAzzimonti <122782427+MAzzimonti@users.noreply.github.com>
2024-05-31 19:51:16 +0200

    db aggiornato

--- backup-old/backup 0.0.5.sql
+++ backup-old/backup 0.0.5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2024 at 07:49 PM
+-- Generation Time: May 31, 2024 at 12:02 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
